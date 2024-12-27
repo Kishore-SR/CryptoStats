3fdd88a76ea2d1c97c59e17935f7ef907da62298
Kishore S R <kishoresr01@gmail.com>
2024-12-27 17:14:56 -0500

    add: Home section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
 
 export const Home = () => {
-  return 
-  (
-    <div>Home</div>
-  )
-}
+  return (
+    <div className="home">
+      <div className="hero">
+        <h1>Track Cryptocurrency <br/>Prices and More</h1>
+        <p>
+          Get real-time prices, 24-hour changes, market cap, and more for
+          Bitcoin, altcoins, and other cryptocurrencies.
+        </p>
+
+        <form>
+          <input type="text" placeholder="Search crypto here..." />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  );
+};
